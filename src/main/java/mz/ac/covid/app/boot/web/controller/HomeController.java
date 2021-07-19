616c843d59627977c60ba00d3b44480b249d9cb9
helderjosue <helderjosuemata@gmail.com>
2021-07-19 08:40:30 +0000

    adding dashboard

--- src/main/java/mz/ac/covid/app/boot/web/controller/HomeController.java
+++ src/main/java/mz/ac/covid/app/boot/web/controller/HomeController.java
@@ -2,17 +2,19 @@ package mz.ac.covid.app.boot.web.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
+@RequestMapping(path = "/")
 public class HomeController {
 
     @GetMapping("admin")
     public String admin() {
-        return "/iframe";
+        return "iframe";
     }
 
-    @GetMapping("home")
+    @GetMapping("dashboard")
     public String home() {
-        return "user/iframe";
+        return "admin/pages/dashboard/dashboard";
     }
 }
