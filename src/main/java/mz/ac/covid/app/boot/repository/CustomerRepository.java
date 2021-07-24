ff11dbc81531557686dea102c5ec30b9036729fe
helderjosue <helderjosuemata@gmail.com>
2021-07-24 21:57:05 +0000

    adding notificacao em massa form

--- src/main/java/mz/ac/covid/app/boot/repository/CustomerRepository.java
+++ src/main/java/mz/ac/covid/app/boot/repository/CustomerRepository.java
@@ -10,17 +10,21 @@ import mz.ac.covid.app.boot.domain.Customer;
 
 public interface CustomerRepository extends JpaRepository<Customer, Long> {
 
+  @Query(value = "SELECT * FROM customer GROUP BY empresa", nativeQuery = true)
+  public List<Customer> getAllInstitutions();
 
-    @Query(value="SELECT * FROM customer GROUP BY empresa", nativeQuery = true)
-    public List<Customer> getAllInstitutions();
+  @Query(value = "SELECT * FROM customer WHERE empresa=:empresaName GROUP BY sala_vacinacao", nativeQuery = true)
+  public List<Customer> getAllSalas(@Param("empresaName") String empresaName);
 
-    @Query(value="SELECT * FROM customer WHERE empresa=:empresaName GROUP BY sala_vacinacao", nativeQuery = true)
-    public List<Customer> getAllSalas(@Param("empresaName") String empresaName);
+  @Query(value = "SELECT * FROM customer WHERE empresa=:empresaName AND sala_vacinacao=:SalaNumber", nativeQuery = true)
+  public List<Customer> getAllCustomers(@Param("empresaName") String empresaName,
+      @Param("SalaNumber") String SalaNumber);
 
-    @Query(value="SELECT * FROM customer WHERE empresa=:empresaName AND sala_vacinacao=:SalaNumber", nativeQuery = true)
-    public List<Customer> getAllCustomers(@Param("empresaName") String empresaName, @Param("SalaNumber") String SalaNumber);
+  @Query(value = "SELECT * FROM customer WHERE id=:Id", nativeQuery = true)
+  public Customer getCustomerById(@Param("Id") Long Id);
 
-    @Query(value="SELECT * FROM customer WHERE id=:Id", nativeQuery = true)
-    public Customer getCustomerById(@Param("Id") Long Id);
+  @Query(value = "SELECT * FROM customer WHERE estado_vacinacao=:estado AND empresa=:instituicao AND sala_vacinacao=:sala", nativeQuery = true)
+  public List<Customer> search(@Param("estado") Integer estado, @Param("instituicao") String instituicao,
+      @Param("sala") String sala);
 
-  }
+}
